e46827d7fd187298603e6808984a38b5e56a6542
AE SChan <abigailsinchan@gmail.com>
2023-06-12 03:21:06 -0400

    added if statement for password criteria arrays

--- asset/js/pwdgen.js
+++ asset/js/pwdgen.js
@@ -3,26 +3,34 @@ let passwordText = document.querySelector("#password");
 
 function generatePassword() {
     let lengthOption = parseInt(prompt("How many characters would you like your password to contain?"));
+
     if (isNaN(lengthOption) || lengthOption < 8 || lengthOption > 128) {
         alert("Please enter a valid length between 8 and 128 characters.");
         return "";
-      }  
+}  
 }
-
 const includeNumbers = confirm('Click OK to confirm inclusion of numeric characters');
 const includeLowercase = confirm('Click OK to confirm inclusion of lowercase alphabets');
 const includeUppercase = confirm('Click OK to confirm inclusion of uppercase alphabets');
 const includeSymbols = confirm('Click OK to confirm inclusion of special characters');
 
-includeNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let selectedCharacters = [];
 
- includeLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  
+  if (includeNumbers) {
+    let arrayCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  }
 
-includeUppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  if (includeLowercase) {
+    let arrayCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  }
 
+  if (includeUppercase) {
+    let arrayCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+}
 
-includeSymbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ',', '<', '>', '?', ':', '/', '~', '_','-', '=', '+'];
+  if (includeSymbols) {
+    let arrayCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ',', '<', '>', '?', ':', '/', '~', '_','-', '=', '+'];
+  }
 
 
 
