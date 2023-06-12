86514d7458f4dd1a56e142daaea81eff35479538
AE SChan <abigailsinchan@gmail.com>
2023-06-12 02:55:54 -0400

    added confirm boxes and started variables for password criteria

--- asset/js/pwdgen.js
+++ asset/js/pwdgen.js
@@ -6,9 +6,25 @@ function generatePassword() {
     if (isNaN(lengthOption) || lengthOption < 8 || lengthOption > 128) {
         alert("Please enter a valid length between 8 and 128 characters.");
         return "";
-      }
-    
+      }  
 }
+
+const includeNumbers = confirm('Click OK to confirm inclusion of numeric characters');
+const includeLowercase = confirm('Click OK to confirm inclusion of lowercase alphabets');
+const includeUppercase = confirm('Click OK to confirm inclusion of uppercase alphabets');
+const includeSymbols = confirm('Click OK to confirm inclusion of special characters');
+
+includeNumbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+ includeLowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  
+
+includeUppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+
+includeSymbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', ',', '<', '>', '?', ':', '/', '~', '_','-', '=', '+'];
+
+
 
 // Write password to the #password input
 function writePassword() {
